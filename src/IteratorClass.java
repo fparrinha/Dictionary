d824391b4d182426b7c9537aee564f60fff8154a
MAPTIH <martin2000.abv@gmail.com>
2021-12-13 03:22:19 +0000

    iterator done

--- src/IteratorClass.java
+++ src/IteratorClass.java
@@ -3,22 +3,32 @@ public class IteratorClass<E> implements Iterator<E> {
 	// Protected variables
 	protected int i;
 	protected Iterator<E> it;
+	protected int maxSize;
+	@SuppressWarnings("rawtypes")
+	protected SepChainHashTable table;
 
 	// Constructor
-	public IteratorClass() {
+
+	@SuppressWarnings({ "rawtypes" })
+	public IteratorClass(SepChainHashTable table) {
 		rewind();
+		maxSize = table.maxSize;
+		this.table = table;
 	}
 
 	@Override
 	public boolean hasNext() {
-		return i < ;
+		return it.hasNext();
+
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public E next() {
-		
-
-		return ;
+		while (i < maxSize && !table.table[i].iterator().hasNext())
+			i++;
+		it = table.table[i].iterator();
+		return it.next();
 	}
 
 	@Override
